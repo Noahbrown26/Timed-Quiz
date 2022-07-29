e8adc4d86f28b30dc2e030516117783c81ca1a00
Noahb26 <noahbrown2663@gmail.com>
2022-07-28 17:13:34 -0700

    add javascript countdown / restructure files

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,131 @@
 const question = document.getElementById('question');
 const choices = Array.from( document.getElementsByClassName('choice-text'));
-console.log(choices);
+var timerEl = document.getElementById('countdown');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let timer = 45;
+let availableQuestions = [];
+
+    // Question Objects //
+
+let questions = [
+    {
+        question: "Commonly used data types DO Not Inlude:",
+        choice1: "Strings",
+        choice2: "Booleans",
+        choice3: "Alerts",
+        choice4: "Numbers",
+        answer: 3
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with _______.",
+        choice1: "Quotes",
+        choice2: "Curly Brackets",
+        choice3: "Paranthesis",
+        choice4: "Square Brackets",
+        answer: 2
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        choice1: "Numbers and Strings",
+        choice2: "Other Arrays",
+        choice3: "Booleans",
+        choice4: "All of the Above",
+        answer: 4
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        choice1: "Commas",
+        choice2: "Curly Brackets",
+        choice3: "Quotes",
+        choice4: "Paranthesis",
+        answer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger:",
+        choice1: "JavaScript",
+        choice2: "Terminal / Bash",
+        choice3: "For Loops",
+        choice4: "Console.log",
+        answer: 4
+    },
+];
+
+// Function that starts game//
+
+startGame = () => {
+timer = 45;
+availableQuestions = [...questions];
+getNewQuestion();
+};
+
+// function that fetches new questions //
+
+getNewQuestion = () => {
+
+    // send user to end screen if timer or questions run out //
+
+    if(availableQuestions.length === 0 || timer === 0){
+        return window.location.assign('/endscreen.html');
+    }
+
+    // Randomly pulls questions & answers from the arrays and sets the text using the appropriate data numbers //
+
+const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })    
+    
+    // remove most recent question from available questions //
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
+
+    // After answering a question, get a new question //
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        getNewQuestion();
+    });
+});
+
+
+    // Countdown function to tick down the timer every second and also reduce time if the player answers incorrect //
+
+function countdown() {
+    var timeLeft = 44;
+  
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = 'Time: ' + timeLeft;
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = 'Time: ' + timeLeft;
+        timeLeft--;
+      } else {
+        clearInterval(timeInterval);
+        return window.location.assign('/endscreen.html');
+      }
+    }, 1000);
+  }
+
+        // Calling Game Functions //
+
+startGame();
+countdown();
+
+
+
